539bc75fe68a328198cccafb496ce8baa942c947
Matt Dlobik <matt.dlobik@gmail.com>
2020-04-13 21:15:02 -0500

    Have added color variants, images and product details

--- js/main.js
+++ js/main.js
@@ -4,8 +4,20 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: './assets/vmSocks-green.jpg'
-
+        image: './assets/vmSocks-green.jpg',
+        inStock: true,
+        inventory: 100,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+            }
+        ]
     }
 })
 
